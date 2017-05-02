370b3f3c47c9010978392da6c954aa984e0a8b53
lijunbo1018 <lijunbo1018@gmail.com>
2017-05-02 19:19:17 +0800

    syntax learning: basic types part 2

--- syntax/basicTypes.ts
+++ syntax/basicTypes.ts
@@ -13,3 +13,36 @@ I'll be ${ age + 1 } years old next year.`;
 // Array
 let numList: number[] = [1, 2, 3];
 let strList: Array<string> = ['1', '2', '3'];
+// Tuple
+let tuple: [string, number];
+tuple = ['hello', 10];
+// tuple = [10, 'hello']; Error
+console.log(tuple[0].substr(1));
+tuple[3] = 'world';
+// tuple[4] = true; Error, 'boolean' isn't 'string | number'
+// Enum
+enum Color {Red = 1, Green, Blue}
+let color: Color = Color.Green;
+let colorName: string = Color[4] || Color[color];
+console.log(colorName);
+// Any
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+let list: any[] = [1, true, 'free'];
+// Void
+function warnUser(): void {
+    alert('This is warning message');
+}
+// Null and Undefined
+let u: undefined = undefined;
+let n: null = null;
+// let a: number = n; Error when use --strictNullChecks flag; Use union instead
+// Never
+function error(message: string): never {
+    throw new Error(message);
+}
+// Type assertions
+let someValue: any = 'this is a string';
+// let strLength: number = (<string>someValue).length; not allowed in JSX
+let strLength: number = (someValue as string).length
